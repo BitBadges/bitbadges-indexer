3bf2beb3ab4e33d3b17d675670b618a7fcf01868
trevormil <trevormil@comcast.net>
2023-02-15 10:00:06 -0500

    fix accounts not storing

--- src/handlers/handleNewAccount.ts
+++ src/handlers/handleNewAccount.ts
@@ -5,8 +5,8 @@ import { IndexerStargateClient } from "../indexer_stargateclient";
 export const handleNewAccount = async (accountNum: number, client: IndexerStargateClient): Promise<void> => {
     const docs: Docs = await fetchDocsForRequest([accountNum], []);
 
-    if (!docs.accounts[accountNum]) {
-        let accountInfo = await client.badgesQueryClient?.badges.getAccountInfo(Number(accountNum))
+    if (!docs.accounts[accountNum].address) { //is just a template
+        let accountInfo = await client.badgesQueryClient?.badges.getAccountInfoByNumber(Number(accountNum))
 
         if (accountInfo) {
             docs.accounts[accountNum] = {
