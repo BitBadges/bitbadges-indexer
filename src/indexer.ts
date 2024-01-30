03edafda19cc8056dcd500972f27bd858407422d
Trevor Miller <trevormil@comcast.net>
2024-01-30 12:22:14 -0500

    .

--- src/indexer.ts
+++ src/indexer.ts
@@ -415,23 +415,23 @@ const init = async () => {
 
 
 const server = process.env.DISABLE_API === 'true' ? undefined :
-  https.createServer(
-    {
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert"),
-    },
-    app
-  ).listen(port, () => {
+  // https.createServer(
+  //   {
+  //     key: fs.readFileSync("server.key"),
+  //     cert: fs.readFileSync("server.cert"),
+  //   },
+  //   app
+  // ).listen(port, () => {
+  //   init().catch(console.error).then(() => {
+  //     console.log(`\nserver started at http://localhost:${port}`, Date.now().toLocaleString());
+  //   })
+  // })
+
+  app.listen(port, () => {
     init().catch(console.error).then(() => {
       console.log(`\nserver started at http://localhost:${port}`, Date.now().toLocaleString());
     })
   })
-
-// app.listen(port, () => {
-//   init().catch(console.error).then(() => {
-//     console.log(`\nserver started at http://localhost:${port}`, Date.now().toLocaleString());
-//   })
-// })
 
 
 
